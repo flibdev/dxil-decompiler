019a8f1ccaaf99f3f129693e3f2a2e40a6fbf936
Reece Sheppard <reece@flib.com.au>
2025-12-27 20:02:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dxil-decompiler
+
---
+++ dxil_decompiler.cpp
@@ -0,0 +1,21 @@
+﻿// DXIL_Decompiler.cpp : Defines the entry point for the application.
+//
+
+#include "dxil_decompiler.h"
+
+#include <stdio.h>
+
+
+#define WRITE_ERR(s) snprintf(buffer, size-1, s);
+
+DXIL_DECOMPILER_API void dxd_get_error_string(error_id id, char* buffer, size_t size) {
+	switch (id)
+	{
+
+	case 0:
+	default:
+		WRITE_ERR("No error.");
+		break;
+	}
+}
+
